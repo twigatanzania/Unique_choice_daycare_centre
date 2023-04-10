2b9d18e8ab2c7dfc620479b741be82210e6fcd97
twigatanzania <twiga.tanzania.website@gmail.com>
2023-04-10 18:19:20 +0300

    added files for the first time

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+
+	<head>  
+	<title>Javascript_Lessons</title>  
+
+	<link rel="stylesheet" href="css_files/maincssfile.css">
+
+	<script src="js_files/Jquery_3.6.js"></script>
+	<script src="js_files/myjs.js"></script>
+	</head>  
+
+<div class="titlediv">
+	  <img id="titleimg" src="images/ucdc.jpg">
+	  <h1 id="title">UNIQUE CHOICE DAYCARE CENTRE</h1>
+	  </div>
+	  <div class="header" id="myHeader">
+	  <a href="index.php">Home</a>
+	  <a href="about.php">About</a>
+	  <a href="Support.php">Support</a>
+	  <a href="gallery.php">Gallery</a>
+	  <a href="contacts.php">Contacts</a>
+	  </div>
